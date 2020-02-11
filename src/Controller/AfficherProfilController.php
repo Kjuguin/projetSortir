3d696c184bea707cbacc49d13808d6174899cac7
Clement-L44 <clemlehuede@gmail.com>
2020-02-11 16:00:59 +0100

    1.Controller AfficherProfilController
    2.Twig: 1-Index -> affichage avec des liens des différents User dans la BDD                  (pour le test.
            2-AfficherProfil -> affichageProfil du profil sélectionné.

---
+++ src/Controller/AfficherProfilController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AfficherProfilController extends AbstractController
+{
+
+    /**
+     * @Route("/profil/index", name="profilIndex")
+     * @param EntityManagerInterface $em
+     * @return Response
+     */
+
+    public function index(EntityManagerInterface $em){
+
+        $userRepository = $em->getRepository(User::class);
+        $user = $userRepository->findAll();
+
+        return $this->render('afficher_profil/index.html.twig',['user'=>$user]);
+    }
+
+
+    /**
+     * @Route("/profil/{id}", name="afficherProfil", requirements={"id"="\d+"})
+     * @param int $id
+     * @param EntityManagerInterface $em
+     * @return Response
+     */
+    public function detail(int $id, EntityManagerInterface $em)
+    {
+        $profileRepository = $em->getRepository(User::class);
+        $profile = $profileRepository->find($id);
+        return $this->render('afficher_profil/afficherProfil.html.twig', ['profile'=>$profile]
+        );
+    }
+}
