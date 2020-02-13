127cd087073dec2ab4727bdc0b3a0ff000d5dbb6
Clement-L44 <clemlehuede@gmail.com>
2020-02-13 18:41:26 +0100

    az

--- src/Controller/AfficherProfilController.php
+++ src/Controller/AfficherProfilController.php
@@ -6,6 +6,7 @@ use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class AfficherProfilController extends AbstractController
 {
