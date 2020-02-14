babefaf6347426150eed6df6f01792eeff3fd72f
Grimmby <camillec-44@hotmail.fr>
2020-02-14 16:24:19 +0100

    css avancé

--- public/js/home.js
+++ public/js/home.js
@@ -12,3 +12,16 @@ $(document).ready(function() {
     });
     $('.dataTables_length').addClass('bs-select');
 });
+
+$('.switch-input').change(function(e){
+    var etat =$(this).is(":checked");
+    var value= this.value;
+    e.preventDefault();
+    $.ajax({
+        url: $('#urlModifierWs').val(),
+        type: 'POST',
+        data: 'value=' + value + '&etat=' + etat,
+        success: function() {
+        }
+    });
+});
