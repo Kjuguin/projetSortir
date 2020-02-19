b5c4fa70b98b55dffa7c558603db3a126477fdb0
Ephese <onasa@hotmail.fr>
2020-02-19 10:21:21 +0100

    Tests fonctionnels: -affichage et redirection des pages

--- tests/Controller/ControllerTest.php
+++ tests/Controller/ControllerTest.php
@@ -14,7 +14,7 @@ use Symfony\Component\DomCrawler\Crawler;
 use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 use Symfony\Component\Security\Http\Event\InteractiveLoginEvent;
 
-class ProfilControllerTest extends WebTestCase
+class ControllerTest extends WebTestCase
 {
 
     /**
@@ -53,15 +53,42 @@ class ProfilControllerTest extends WebTestCase
         $cookie = new Cookie($session->getName(), $session->getId());
         $this->client->getCookieJar()->set($cookie);
     }
+    /**
+     * Méthode pour enregistrer une authentification d'admin
+     */
+    private function logInAdmin()
+    {
+        /** @var EntityManagerInterface $entityManager */
+        $entityManager = $this->client->getContainer()
+            ->get('doctrine')
+            ->getManager();
+        $session = $this->client->getContainer()->get('session');
+        $securityTokenStorage = $this->client->getContainer()->get('security.token_storage');
+        $eventDispatcher = $this->client->getContainer()->get('event_dispatcher');
+        $user = $entityManager->getRepository(User::class)->find(157);
+        $firewallName = 'main';
+        // if you don't define multiple connected firewalls, the context defaults to the firewall name
+        // See https://symfony.com/doc/current/reference/configuration/security.html#firewall-context
+        $firewallContext = 'main';
+        // you may need to use a different token class depending on your application.
+        // for example, when using Guard authentication you must instantiate PostAuthenticationGuardToken
+        $token = new UsernamePasswordToken($user, null, 'main', $user->getRoles());
+        $securityTokenStorage->setToken($token);
+        $session->set('_security_main', serialize($token));
+        $session->save();
+        $cookie = new Cookie($session->getName(), $session->getId());
+        $this->client->getCookieJar()->set($cookie);
+    }
+
+    /********************* Tests de redirection ou d'affichage *********************/
 
     /**
-     * Test de redirection si utilisateur non connecté
+     * Test de redirection de la page home si utilisateur non connecté
      */
     public function testRedirectHomeIfNotConnected()
     {
-        $crawler = $this->client->request('GET', '/home');
-        $this->client->followRedirect();
-        $this->assertSame(1,$crawler->filter('html:contains("login")')->count());
+        $this->client->request('GET', '/');
+        $this->assertSame(302, $this->client->getResponse()->getStatusCode());
     }
 
     /**
@@ -70,19 +97,18 @@ class ProfilControllerTest extends WebTestCase
     public function testHomepageIsUp()
     {
         $this->logIn();
-        $crawler = $this->client->request('GET', '/home');
+        $crawler = $this->client->request('GET', '/');
         //echo $this->client->getResponse()->getContent();
         $this->assertSame(1,$crawler->filter('html:contains("Le nom contient")')->count());
     }
 
     /**
-     * Test de redirection si utilisateur non connecté
+     * Test de redirection afficher profil si utilisateur non connecté
      */
     public function testRedirectAfficherProfilIfNotConnected()
     {
-        $crawler = $this->client->request('GET', '/utilisateur/afficherProfil/164');
-        $this->client->followRedirect();
-        $this->assertSame(1,$crawler->filter('html:contains("login")')->count());
+        $this->client->request('GET', '/utilisateur/afficherProfil/164');
+        $this->assertSame(302, $this->client->getResponse()->getStatusCode());
     }
 
     /**
@@ -94,6 +120,46 @@ class ProfilControllerTest extends WebTestCase
         $crawler = $this->client->request('GET', '/utilisateur/afficherProfil/157');
         $this->assertSame(200, $this->client->getResponse()->getStatusCode());
     }
+
+    /**
+     * Test de redirection afficher sortie si utilisateur non connecté
+     */
+    public function testRedirectAfficherSortieIfNotConnected()
+    {
+        $this->client->request('GET', '/sortie/afficherSortie/264');
+        $this->assertSame(302, $this->client->getResponse()->getStatusCode());
+    }
+
+    /**
+     * Test affichage de la page afficher sortie si utilisateur connecté
+     */
+    public function testAfficherSortieIsUp()
+    {
+        $this->logIn();
+        $crawler = $this->client->request('GET', '/sortie/afficherSortie/264');
+        $this->assertSame(200, $this->client->getResponse()->getStatusCode());
+    }
+
+    /**
+     * Test de redirection inscription sortie si utilisateur non admin
+     */
+    public function testRedirectInscriptionSortieIfNotAdmin()
+    {
+        $this->client->request('GET', '/registration');
+        $crawler = $this->client->followRedirect();
+        $this->assertSame(1,$crawler->filter('html:contains("login")')->count());
+    }
+
+    /**
+     * Test affichage de la page inscription sortie si utilisateur admin
+     */
+    public function testInscriptionSortieIfAdminIsUp()
+    {
+        $this->logInAdmin();
+        $this->client->request('GET', '/registration');
+        $this->assertSame(200, $this->client->getResponse()->getStatusCode());
+    }
+
 
     /**
      * Test de validation du formulaire de gestion d'un profil
@@ -115,14 +181,6 @@ class ProfilControllerTest extends WebTestCase
         //echo $this->client->getResponse()->getContent();
     }*/
 
-    /**
-     * Test de redirection si utilisateur non connecté
-     */
-    public function testRedirectAfficherSortieIfNotConnected()
-    {
-        $crawler = $this->client->request('GET', '/sortie/afficherSortie/264');
-        $this->client->followRedirect();
-        $this->assertSame(1,$crawler->filter('html:contains("login")')->count());
-    }
+
 
 }
