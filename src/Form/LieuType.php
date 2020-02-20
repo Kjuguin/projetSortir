7cd6aa8c35558e49157c0c92b1cb24959c854d24
kjuguin2019 <kevin.juguin2019@campus-eni.fr>
2020-02-20 17:26:05 +0100

    controle

--- src/Form/LieuType.php
+++ src/Form/LieuType.php
@@ -6,6 +6,7 @@ use App\Entity\Lieu;
 use App\Entity\Ville;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
