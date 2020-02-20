194ea0d15b11f02dabddda108e75c28140485d1c
Clement-L44 <clemlehuede@gmail.com>
2020-02-20 18:11:45 +0100

    JS Gérer Site

--- src/Form/AjoutSiteType.php
+++ src/Form/AjoutSiteType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Site;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,7 +13,14 @@ class AjoutSiteType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('nomSite')
+            ->add('nomSite', TextType::class,[
+                'attr'=>[
+                    'id'=>'idNomSite',
+                    'class'=>'nomSite'
+                    ],
+                'label'=>'Nom Site:',
+                'label_attr'=>['class'=>'labelNomSite'],
+            ])
         ;
     }
 
