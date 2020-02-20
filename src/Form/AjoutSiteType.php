ad72c18de84f723ec17e6a672d317ed05299adc6
Grimmby <camillec-44@hotmail.fr>
2020-02-20 19:06:16 +0100

    Branche Over 90000000000 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- src/Form/AjoutSiteType.php
+++ src/Form/AjoutSiteType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Site;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,8 +13,13 @@ class AjoutSiteType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('nomSite')
-        ;
+            ->add('nomSite', TextType::class,
+                [
+                  'label' => 'Nom du site',
+                  'label_attr'=>['class'=>'label-lieu'],
+                   'attr'=>['class'=>'input-lieu']
+                ]
+            );
     }
 
     public function configureOptions(OptionsResolver $resolver)
